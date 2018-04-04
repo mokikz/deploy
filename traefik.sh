ceeaacfc79229bee4d2ffbf1cafa68a9d55db130
WolfgangLang <kristina.und.wolfgang@t-online.de>
2018-04-05 01:32:05 +0200

    adopted config

--- traefik.sh
+++ traefik.sh
@@ -1,11 +1,1 @@
-docker run -d \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  -v $PWD/traefik.toml:/traefik.toml \
-  -v $PWD/acme.json:/acme.json \
-  -p 80:80 \
-  -p 443:443 \
-  -l traefik.frontend.rule=Host:monitor.mokikz.de \
-  -l traefik.port=8080 \
-  --network proxy \
-  --name traefik \
-  traefik:1.3.6-alpine --docker
+docker run -d -v /var/run/docker.sock:/var/run/docker.sock -v $PWD/traefik.toml:/traefik.toml -v $PWD/acme.json:/acme.json -v $PWD/traefik.log:/var/log/traefik/traefik.log -p 80:80 -p 443:443 -p 8080:8080 -l traefik.frontend.rule=Host:monitor.mokikz.de  -l traefik.port=8080 --network proxy traefik
